4f59d3e30f8a781be757dd041b3d9d6d35c9262b
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-08-25 16:53:01 -0700

    Update script.js

--- search/script.js
+++ search/script.js
@@ -1,5 +1,9 @@
-let parameter = prompt("Please enter a Project ID (keep the slash)", "/");
-window.location = "https://scratchbackup.github.io/projects" + parameter;
-document.open("index.html","replace");
-document.write("<p>bruh you're not supposed to see this</p>");
-document.close();
+window.setTimeout(search, 1000);
+
+function search()  {
+  let parameter = prompt("Please enter a Project ID (keep the slash)", "/");
+  window.location = "https://scratchbackup.github.io/projects" + parameter;
+  document.open("index.html","replace");
+  document.write("<p>bruh you're not supposed to see this</p>");
+  document.close();
+}
