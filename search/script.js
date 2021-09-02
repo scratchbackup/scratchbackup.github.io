00d8d3e3140bac5bc0aacd938bed41545e83675a
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-09-01 17:20:47 -0700

    Update script.js

--- search/script.js
+++ search/script.js
@@ -1,7 +1,4 @@
 function search() {
   let parameter = document.forms["srch"]["param"].value;
   window.location = "https://scratchbackup.github.io/projects/" + parameter + "/project.json";
-  document.open("index.html", "replace");
-  document.write("<p>bruh you're not supposed to see this</p>");
-  document.close();
 }
