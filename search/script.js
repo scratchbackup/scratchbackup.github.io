c1a755da5010e63669e64cd34a4f77d38ff30755
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-09-04 11:25:13 -0700

    Update script.js

--- search/script.js
+++ search/script.js
@@ -1,7 +1,6 @@
-function search() {
-  let parameter = 
-  var linky = window.location.href
-  var url = new URL(linku);
-  var res = url.searchParams.get("param");
+var linky = window.location.href
+var url = new URL(linku);
+var res = url.searchParams.get("param");
+if res.length > 1
   window.location = "https://scratchbackup.github.io/projects/" + res + "/project.json";
 }
