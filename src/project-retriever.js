6bb3575a001d2fffe47fffcea6433186c3713b10
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-10-04 19:59:34 -0700

    Update project-retriever.js

--- src/project-retriever.js
+++ src/project-retriever.js
@@ -1,3 +1,27 @@
 console.log("script loaded");
 const https = require("https");
 const fs = require("fs");
+
+// borrowed a lot of code from functionalmetatable
+
+function getJSON(url) {
+  return new Promise((resolve, reject) => {
+    const urlobj = new URL(url);
+    const req = https
+      .request(urlobj, (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+          resolve(JSON.parse(data));
+        });
+
+        res.on("error", (err) => {
+          reject(err);
+        });
+      })
+      .end();
+  });
+}
