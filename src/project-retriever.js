5977fe20a6eb87510f3714aeed080ecab1263bb1
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-10-07 13:48:00 -0700

    Update project-retriever.js

--- src/project-retriever.js
+++ src/project-retriever.js
@@ -1,21 +1,29 @@
 console.log("script loaded");
 
-$.get("projects.json", function(data){
-  const projects = json.parse(data);
-});
-var thumblists = [];
-var links = [];
-for (let projectID of projects) {
-  var projectthumburl = new URL("https://scratchbackup.github.io/projects/" + projectID + "/thumbnail.png");
-  var projectfile = new URL("https://scratchbackup.github.io/projects/" + projectID + "/project.json");
-  var thumblists += projectthumburl;
-  var links += projectfile;
-}
+$(document).ready(function(){
+  $.get("https://scratchbackup.github.io/src/projects.json", function(data){
+    const projects = json.parse(data);
+  });
+  
+  const thumblists = [];
+  
+  const links = [];
+  for (let projectID of projects) {
+    var projectthumburl = new URL("https://scratchbackup.github.io/projects/" + projectID + "/thumbnail.png");
+    var projectfile = new URL("https://scratchbackup.github.io/projects/" + projectID + "/project.json");
+    thumblists.push(projectthumburl);
+    links.push(projectfile);
+  }
+  var indexx = 0;
+  for (let link of links) {
+    var indexx += 1
+    document.getElementById("project" + indexx + "link").href = link;
+  }
+  var indexx = 0;
+  for (let thumb of thumblists) {
+    var indexx += 1
+    document.getElementById("project" + indexx + "thumb").src = thumb;
+  }
+  console.log("complete");
 
-for (let link of links) {
-  document.getElementById("project1link").href = link;
-}
-for (let thumb of thumblists) {
-  document.getElementById("project1thumb").src = thumb;
-}
-console.log("complete");
+});
