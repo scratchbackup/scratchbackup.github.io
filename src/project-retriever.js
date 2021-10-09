afbea960ade7a8b605c8d70f842618eedf812637
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-10-08 17:22:56 -0700

    Update project-retriever.js

--- src/project-retriever.js
+++ src/project-retriever.js
@@ -1,29 +1,34 @@
 console.log("script loaded");
+var projects;
+// Create an XMLHttpRequest object
+const xhttp = new XMLHttpRequest();
 
-$(document).ready(function(){
-  $.get("https://scratchbackup.github.io/src/projects.json", function(data){
-    const projects = json.parse(data);
-  });
-  
-  const thumblists = [];
-  
-  const links = [];
-  for (let projectID of projects) {
-    var projectthumburl = new URL("https://scratchbackup.github.io/projects/" + projectID + "/thumbnail.png");
-    var projectfile = new URL("https://scratchbackup.github.io/projects/" + projectID + "/project.json");
-    thumblists.push(projectthumburl);
-    links.push(projectfile);
-  }
-  var indexx = 0;
-  for (let link of links) {
-    var indexx += 1
-    document.getElementById("project" + indexx + "link").href = link;
-  }
-  var indexx = 0;
-  for (let thumb of thumblists) {
-    var indexx += 1
-    document.getElementById("project" + indexx + "thumb").src = thumb;
-  }
-  console.log("complete");
+// Define a callback function
+xhttp.onload = function() {
+  var projects = xhttp.responseText;
+}
 
-});
+// Send a request
+xhttp.open("GET", "src/projects.json");
+xhttp.send();
+const thumblists = [];
+  
+const links = [];
+for (let projectID of projects) {
+  var projectthumburl = new URL("https://scratchbackup.github.io/projects/" + projectID + "/thumbnail.png");
+  var projectfile = new URL("https://scratchbackup.github.io/projects/" + projectID + "/project.json");
+  thumblists.push(projectthumburl);
+  links.push(projectfile);
+}
+var indexx = 0;
+for (let link of links) {
+  var indexx += 1
+  document.getElementById("project" + indexx + "link").href = link;
+}
+var indexx = 0;
+for (let thumb of thumblists) {
+  var indexx += 1
+  document.getElementById("project" + indexx + "thumb").src = thumb;
+}
+console.log("complete");
+
