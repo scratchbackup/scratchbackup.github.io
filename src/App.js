bf57f68d3affe37be829f4ace3bbc81e1a3fcc43
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-11-17 08:34:48 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -62,14 +62,6 @@ class App extends Component {
             Scratch.
           </p>
 
-          <h3>How do I get my project added?</h3>
-          <!-- <p>
-            We have a{" "}
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLScMXAtkgyMDAWvrDvLAg6D0elCOWg1mrBEFsi1AcwtE0zD3vw/viewform">
-              web form
-            </a>{" "}
-            that you can fill out.
-          </p> --->
           <h3>What is the .sbcomp format?</h3>
           <p>.sbcomp is a custom file extension name that stands for "Scratch Binary Compression", so you don't accidentally open your file in a different application. You can still upload the file to a Scratch online editor, and all your project's code and assets will be there.
           </p>
