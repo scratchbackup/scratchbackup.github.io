c04a791f72ee0729be36783d53ff74645c71908b
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-12-07 10:09:35 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ class App extends Component {
                       <tr key={project.id}>
                         <td>{showID ? project.id : "-"}</td>
                         <td>{project.title}</td>
-                        <td style={project.color}><a href={`https://scratch.mit.edu/users/${project.author}`}><img src={project.pfp.replace("60x60", "45x45")}></img><span className="verticalcenter">{project.author}</span></a></td>  
+                        <td style={{backgroundColor: project.color}}><a href={`https://scratch.mit.edu/users/${project.author}`}><img src={project.pfp.replace("60x60", "45x45")}></img><span className="verticalcenter">{project.author}</span></a></td>  
                         <td>
                           {created.toLocaleDateString()}{" "}
                           {created.toLocaleTimeString()}
