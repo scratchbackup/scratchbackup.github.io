20b50091c9dcfeb73dd18c157e99dba3c05c7897
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-01-11 09:50:04 -0800

    support unshared projects

--- scripts/add-project.js
+++ scripts/add-project.js
@@ -9,7 +9,7 @@ const id = process.env.PROJECT_ID || process.argv.slice(2)[0]
 if (!id) throw new Error('A project ID was not provided to the client.')
 if (projectSet.has(id)) throw new Error("This project already is being listened to")
 
-fetch(`https://api.scratch.mit.edu/projects/${id}/`)
+fetch(`https://projects.scratch.mit.edu/${id}`)
   .then((res) => {
     if (!res.ok) throw new Error(`Cannot fetch ${res.url} because of error ${res.status}`)
     console.log("Archiving: ", id);
