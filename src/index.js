b76e15774c29d4e94b0a5c08daf62212a9192f2d
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-03-01 08:32:59 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import { App } from './App';
 
 import './scss/main.scss'
 
-ReactDOM.render(<App />, document.querySelector('#entrypoint'))
+try { ReactDOM.render(<App />, document.querySelector('#entrypoint')) }
+catch(e) { ReactDOM.render(<p>The site crashed. The reason was due to an Error: {e} </p>, document.querySelector('#entrypoint')) }
